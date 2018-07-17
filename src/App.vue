8cffd3b6b7b989174f4bbea58fadda9792062ab5
geooooo <geo97it@mail.com>
2018-07-17 11:15:58 +0300

    init vue app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+    text
+</div>
+</template>
+
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>
+
+
+<style scoped lang="scss">
+</style>
